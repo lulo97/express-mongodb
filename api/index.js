64494cd315ea9c28f0dd7703ba257dbbe92acb5e
lulo97 <luongpysl2@gmail.com>
2024-03-01 21:50:20 +0700

    fix

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const User = require('./User.js')
+const User = require('../User.js')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
