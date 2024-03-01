90a5c8bd8e3fe757c16f0de0e56096f8336393cb
lulo97 <luongpysl2@gmail.com>
2024-03-01 22:48:14 +0700

    commit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const User = require('../User.js')
+const User = require('./User.js')
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// MongoDB connection
-const connect_uri = "mongodb+srv://luongpysl:luong123@mycluster.wjbeczh.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connect(connect_uri)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(error => console.error('Error connecting to MongoDB:', error));
+const connectDB = require("./mongoose.js");
+connectDB();
 
 app.use(bodyParser.json());
 
